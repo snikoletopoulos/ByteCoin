636e8fd466b883bd01bf91c3f1e2b532db6d1caa
Stavros <snikoletopoulos@hotmail.com>
2024-04-20 18:33:08 +0300

    Get the value from picker

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -1,8 +1,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var bitcoinLabel: UILabel!
+  @IBOutlet var currencyLabel: UILabel!
+  @IBOutlet var currencyPicker: UIPickerView!
+
+  let coinManager = CoinManager()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
+    currencyPicker.dataSource = self
+    currencyPicker.delegate = self
+  }
+}
+
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDataSource {
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return coinManager.currencyArray.count
+  }
+}
+
+// MARK: - UIPickerViewDataSource
+
+extension ViewController: UIPickerViewDelegate {
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return coinManager.currencyArray[row]
+  }
+
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    let currency = coinManager.currencyArray[row]
+    coinManager.getCoinPrice(for: currency)
   }
 }
